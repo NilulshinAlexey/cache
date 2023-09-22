134d3d1086287e306f3cf7885fde9fd4ca065d31
lexus <nikulshin@phystech.edu>
2023-09-23 00:39:36 +0300

    broken cache

--- cache.cpp
+++ cache.cpp
@@ -2,5 +2,31 @@
 
 int main(){
 
+    int num_page = 0, size = 0, elem, heat = 0;
+    CacheList<int> cache(5);
+
+    cout << "Enter page number: ";
+    cin >> num_page;
+    cout << "Enter cache size: ";
+    cin >> size;
+
+    vector<Node<int> *> CacheTable(size);
+    while(1){
+
+        cin >> elem;
+        if (elem < 0) break;
+
+        if (CacheTable[elem] == NULL) cache.pushtop(CacheTable, 0, elem);
+        else{ 
+         
+            cache.movetop(CacheTable, 0, elem);
+            heat++;
+        }
+        cache.desplayList();    
+    }
+
+    cout << "Number of heat: " << heat << endl;
+    cache.clear();
+
     return 0;
 }
